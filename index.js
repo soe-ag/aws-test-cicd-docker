4d05b1f00fe7a5f1d2c29596e632ab19238090ac
soeaung94 <aung.soe@triple-four.com>
2025-07-09 15:06:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hell from my containerized Noed.js app");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
