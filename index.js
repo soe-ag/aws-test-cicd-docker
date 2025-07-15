f55b2b2cc1a9b57da2275081f08ea20cd827ee47
soeaung94 <aung.soe@triple-four.com>
2025-07-15 11:58:11 +0900

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from my containerized Node.js app..,new task");
+  res.send("Hello from my containerized Node.js app..");
 });
 
 app.listen(port, () => {
